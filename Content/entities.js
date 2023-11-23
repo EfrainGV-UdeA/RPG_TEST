b9b9ae27306d3da380ce8efecaf14863f5b9e6a6
EfrainGV-UdeA <efrain.garciav@udea.edu.co>
2023-11-23 02:59:03 -0500

    feat: isolated the game contents on their own individual file

---
+++ Content/entities.js
@@ -0,0 +1,28 @@
+window.EntityRace = {
+    human: "human",
+    elf: "elf",
+    dwarf: "dwarf",
+    goblin: "goblin",
+    shadow_kin: "shadow"
+}
+
+window.Entities = {
+    "npcShadowBandit": {
+        name: "Shadow Bandit",
+        race: EntityRace.shadow_kin,
+        src: "assets/sprites/NPC_ShadowBandit.png",
+        icon: "assets/sprites/testIcon1.png",
+    },
+    "npcHumanRed": {
+        name: "Human",
+        race: EntityRace.human,
+        src: "assets/sprites/NPC_HumanRed.png",
+        icon: "assets/sprites/testIcon2.png",
+    },
+    "npcPriest": {
+        name: "Priest",
+        race: EntityRace.elf,
+        src: "assets/sprites/PriestPlayer.png",
+        icon: "assets/sprites/testIcon3.png",
+    }
+}
